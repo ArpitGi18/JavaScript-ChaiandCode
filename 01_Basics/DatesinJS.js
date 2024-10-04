3cd1bd1a892a085aae4dfed91e3c1a83ca539734
ArpitGi18 <125663866+ArpitGi18@users.noreply.github.com>
2024-10-04 10:03:09 +0000

    DatesinJS

---
+++ 01_Basics/DatesinJS.js
@@ -0,0 +1,28 @@
+// let mydate = new Date();
+// console.log(mydate);
+// console.log(mydate.toDateString());
+// console.log(mydate.toLocaleString());
+// console.log(typeof(mydate));
+
+// let mycreateddate = new Date(2023 , 0 ,  23 ,5 , 3);
+// console.log(mycreateddate);
+// console.log(mycreateddate.toDateString());
+// console.log(mycreateddate.toLocaleString());
+
+// let mycreateddate = new Date("2023-05-18");
+// console.log(mycreateddate);
+// console.log(mycreateddate.toLocaleString());
+
+// let mytimestap = Date.now();
+// console.log(mytimestap);
+
+// console.log(mycreateddate.getTime());
+// console.log(Date.now());
+
+// console.log(Math.floor(Date.now()/1000))
+
+let newDate = new Date()
+console.log(newDate)
+console.log(newDate.getDay())
+console.log(newDate.getMonth())
+console.log(newDate.getFullYear())
